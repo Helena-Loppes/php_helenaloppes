e83b455b4c4282cfeeb1606c669996882361f2af
Helena Lopes <helena.lloppes@gmail.com>
2025-07-28 17:11:23 -0300

    2

---
+++ conexaoBanco/conexaoPdo/deletarCliente.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>excluir cliente</title>
+</head>
+<body>
+    <h2>excluir cliente</h2>
+    <form action="processarDelecao.php" method="POST">
+    <label for="number" id="id" name="id" required>
+
+    <button type="submit">Excluir Cliente</button>
+</form>
+</body>
+</html>
